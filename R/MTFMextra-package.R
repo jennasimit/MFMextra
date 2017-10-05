068447a873731361d769b99cce9691c6d8dece0e
Jennifer Asimit <ja628@cam.ac.uk>
2017-10-05 16:02:15 +0100

    fixed library dependencies

--- R/MTFMextra-package.R
+++ R/MTFMextra-package.R
@@ -1,14 +1,10 @@
-#' MTFMextra
-#'
-#' @name MTFMextra
-#' @doctype package
 #' @useDynLib MTFMextra
 #' @importFrom Rcpp evalCpp sourceCpp
 #' @importFrom methods as
 #' @importFrom stats dbinom model.matrix sd terms
 #' @importFrom utils combn write.table read.table data
 #' @importFrom mlogitBMA mlogit2logit
-#' @importFrom snpStats ld col.summary write.SnpMatrix row.summary
+#' @import snpStats
 #' @import Matrix
 #' @import MTFM
 #' @import RcppArmadillo
