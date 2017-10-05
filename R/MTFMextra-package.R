3e93db4253b1d46928772b6710e53ae396fd8aca
Jennifer Asimit <ja628@cam.ac.uk>
2017-10-05 14:24:59 +0100

    R package dependencies fixed

---
+++ R/MTFMextra-package.R
@@ -0,0 +1,10 @@
+#'@useDynLib MTFMextra
+#'@importFrom Rcpp evalCpp sourceCpp
+#' @importFrom methods as
+#' @importFrom stats dbinom model.matrix sd terms
+#' @importFrom utils combn write.table
+#' @importFrom mlogitBMA mlogit2logit
+#' @import snpStats
+#' @import MTFM
+#' @import RcppArmadillo
+NULL
